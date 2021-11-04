c61c1cb134cf5cc8f2f5ca9e628dfc2a9ee721da
Filipe Melo <filipexpert63@gmail.com>
2021-11-04 16:36:47 -0300

    Finalização da resolução do bug de pagina.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Characters from "./components/Characters";
 function App() {
   const [characterList, setCharacterList] = useState([]);
   const [next, setNext] = useState(1);
+  const [isNext, setIsNext] = useState(true);
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${next}`)
       .then((res) => res.json())
-      .then((res) => setCharacterList(res.results))
+      .then((res) => {
+        setCharacterList(res.results);
+        res.info.next === null && setIsNext(false);
+      })
       .catch((error) => console.error(error));
   }, [next]);
   const handleNext = () => {
-    next !== null && setNext(next + 1);
+    isNext && setNext(next + 1);
   };
   const handleBack = () => {
     next >= 2 && setNext(next - 1);
+    isNext === false && setIsNext(true);
   };
+  console.log(next);
   return (
     <div className="App">
       <header className="App-header">
